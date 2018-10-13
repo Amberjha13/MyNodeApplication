4a42d01d6acc1ffe0c85e178143dd3a0b0ab541c
amberjha13 <amberjha13@gmail.com>
2018-10-13 01:00:08 -0700

    my first commit1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get('/',(req,res)=>{
-    console.log("server is starting on",PORT)
+    console.log("server is running on",PORT)
     res.send({"hi":"there "});
 })
 
